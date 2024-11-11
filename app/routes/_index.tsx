c0a440415f8a15e2707ff0e4ba18e1160829fffe
mpassava <mpassava@gmail.com>
2024-11-10 22:40:24 -0500

    installed shadCN and created text animation

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,14 +3,24 @@ import type { MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = () => {
   return [
     { title: "Mike Passavanti Portfolio" },
-    { name: "description", content: "Software Developer Portfolio For Mike Passavanti" },
+    {
+      name: "description",
+      content: "Software Developer Portfolio For Mike Passavanti",
+    },
   ];
 };
 
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <pre id="name-title">Mike Passavanti     Full Stack Developer</pre>
+      <div className="relative flex flex-row justify-center">
+        <div
+          className="text-xl font-bold overflow-hidden whitespace-nowrap mx-0 my-auto tracking-widest animate-typing"
+        >
+          <pre>Mike Passavanti   Full Stack Developer</pre>
+        </div>
+        <div className="border-orange-600 border-r-2 animate-blink-caret"></div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
@@ -16,7 +17,76 @@ export default {
           "Noto Color Emoji",
         ],
       },
+      animation: {
+        typing: "typing 3.5s steps(40, end)",
+        "blink-caret": "blink-caret 0.75s step-end infinite",
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        "blink-caret": {
+          "0%, 100%": {
+            borderColor: "transparent",
+          },
+          "50%": {
+            borderColor: "orange",
+          },
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
